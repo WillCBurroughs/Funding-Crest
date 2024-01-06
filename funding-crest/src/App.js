772bbd6a3e4906327cdd04ee051b10a1c03e6c88
William Burroughs <willcburroughs@gmail.com>
2024-01-06 15:14:49 -0600

    Added page for Ten Best places to apply for grant

--- funding-crest/src/App.js
+++ funding-crest/src/App.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import myImage from "./images/FC.svg";
 import Blog from "./pages/Blog";
+import TenBestPlacesToApplyForGrants from "./pages/TenBestPlacesToApplyForGrants"
 import FirstStory from "./pages/FirstStory"
 import CompetingAtPitchComp from "./pages/CompetingAtPitchComp";
 import { Route, Routes} from "react-router-dom";
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element = {<Blog/>} ></Route>
         <Route path="/FirstStory" element = {<FirstStory/>}> </Route>
         <Route path="/Competing-At-Pitch-Competition-This-Is-What-You-Should-Wear" element = {<CompetingAtPitchComp/>}></Route>
+        <Route path="/Ten-Best-Places-To-Apply-For-Grants" element = {<TenBestPlacesToApplyForGrants/>}></Route>
     </Routes>
   );
 }
---
+++ funding-crest/src/pages/TenBestPlacesToApplyForGrants.js
@@ -0,0 +1,23 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import CustomNavbar from "../components/organisms/CustomNavbar";
+import TopNavbar from "../components/organisms/TopNavbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function TenBestPlacesToApplyForGrants() {
+  return (
+    <div style={{ backgroundColor: "#dee2e6", height: "150vh" }}>
+      {/* <img src={myImage} alt="Funding Crest Logo" /> */}
+
+      <TopNavbar />
+      <CustomNavbar activeLink="blog" />
+      <div className="container-fluid ps-0 pe-0">
+
+        <h1> TenBestPlacesToApplyForGrants </h1>
+
+      </div>
+    </div>
+  );
+}
+
+export default TenBestPlacesToApplyForGrants;
